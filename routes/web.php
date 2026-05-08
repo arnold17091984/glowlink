a4084e805797f9932c0a3e18a9a6355e5ef59cbf
Arnold <arnoldmuran82@gmail.com>
2026-05-08 19:19:21 +0800

    Register LINE webhook routes in routes/web.php at /messages and /messages/{channel}
    Companion to the previous commit which removed the Spatie #[Post]
    attributes from MessagesController. Without explicit route registration
    the controller wasn't reachable. Add web routes that bypass session/CSRF
    middleware (LINE webhooks are stateless signed POSTs).

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,44 @@
 <?php
 
+use App\Http\Controllers\MessagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::redirect('/', '/admin');
+
+/*
+| LINE Webhook
+| - /messages          : default channel (legacy)
+| - /messages/{channel}: per-channel multi-tenant routing
+|
+| Spatie route-attributes は config/route-attributes.php の prefix=api 設定で
+| /api/messages/... に登録してしまうため、ここで明示的に root に登録する。
+| LINE Developers Console の Webhook 検証が想定する URL は /messages/{slug}。
+*/
+Route::post('/messages', MessagesController::class)
+    ->middleware(['line.signature', 'throttle:line-webhook'])
+    ->withoutMiddleware([
+        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+        \Illuminate\Cookie\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    ])
+    ->name('line.webhook.default');
+
+Route::post('/messages/{channel}', MessagesController::class)
+    ->middleware(['line.signature', 'throttle:line-webhook'])
+    ->withoutMiddleware([
+        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+        \Illuminate\Cookie\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    ])
+    ->where('channel', '[a-z0-9\-_]+')
+    ->name('line.webhook.channel');
